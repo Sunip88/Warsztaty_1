edfbf3c13e5a133869ab2d4f0d43b3b4ea092af5
Sunip88 <drzep4@gmail.com>
2018-08-14 18:21:33 +0200

    Guessing game 2 added

---
+++ Gra_w_zgadywanie_liczb_2.py
@@ -0,0 +1,31 @@
+def guessing_2():
+    input("Pomyśl liczbę od 0 do 1000, a ja ją zgadnę w max. 10 próbach.\nJak będziesz gotowy wciśnij enter.")
+    # print("Pomyśl liczbę od 0 do 1000, a ja ją zgadnę w max. 10 próbach")
+    min_num = 0
+    max_num = 1000
+    check = ''
+    guess = 0
+    n = 0
+    while check != 3:
+        temp_guess = guess
+        guess = int((max_num - min_num) / 2) + min_num
+        if temp_guess == guess:
+            print("nie oszukuj")
+            break
+        print(f"Zgaduję: {guess}")
+        n += 1
+        check = input("Zgadłem ? (Wpisz numer opisujący stan: 1: za duzo, 2: za malo, 3: zgadles): ")
+        while not check.isdigit():
+            print("Prosze wybrać liczbę odpowiadającą za stan gry.")
+            check = input("Zgadłem ? (Wpisz numer opisujący stan: 1: za duzo, 2: za malo, 3: zgadles): ")
+        check = int(check)
+        if check == 3:
+            print(f"Wygrałem, potrzebowałem {n} prób")
+        elif check == 1:
+            max_num = guess
+        elif check == 2:
+            min_num = guess
+        else:
+            print("Taka odpowiedź nie istnieję.")
+            break
+guessing_2()
