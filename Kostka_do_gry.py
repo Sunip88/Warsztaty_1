e5ad917b5af7b26ed9769ea44dd9765132b65bf0
Sunip88 <drzep4@gmail.com>
2018-08-24 12:05:52 +0200

    using re fullmatch groups

--- Kostka_do_gry.py
+++ Kostka_do_gry.py
@@ -10,30 +10,26 @@ Application returns 'False' or result of Dice equation (xDy+z as string np.: '2D
 """
 
 
-def Dice(dice_code):
+def dice(dice_code):
     if type(dice_code) == str:
-        if re.match(r'^([1-9][0-9]*)?[D](100|20|12|10|[3468])[+-]?([1-9][0-9]*)?$', dice_code):
-            num_dices_raw = re.search('^([1-9][0-9]{0,6})?[D]', dice_code) # wszystko do D
-            num_dices = num_dices_raw.group(0).replace('D', '')
-            dice_raw = re.search('[D]([1][0][0]|[2][0]|[1][2]|[1][0]|[3468])', dice_code)  # kostka
-            dice = int(dice_raw.group(0).replace('D', ''))
-            dice_mode_raw = re.search('[+-]?([1-9][0-9]*)?$', dice_code)  # modyfikator
-            if dice_mode_raw.group(0) != '':
-                dice_mode = dice_mode_raw.group(0)[1:]
-                dice_mode_sign = dice_mode_raw.group(0)[:1]
-            else:
-                dice_mode = ''
+        pattern = re.compile(r'^([1-9][0-9]*)?[D](100|20|12|10|[3468])([+-])?([1-9][0-9]*)?$')
+        match = pattern.fullmatch(dice_code)
+        if match is not None:
+            num_dices = match.group(1)
+            dice_type = int(match.group(2))
+            dice_mode_sign = match.group(3)
+            dice_mode = match.group(4)
             sum_dice = 0
-            if num_dices != '':
+            if num_dices is not None:
                 num_dices = int(num_dices)
             else:
                 num_dices = 1
             i = 1
             while i <= num_dices:
-                draw = randint(1, dice)
+                draw = randint(1, dice_type)
                 sum_dice += draw
                 i += 1
-            if dice_mode != '':
+            if dice_mode is not None:
                 dice_mode = int(dice_mode)
                 if dice_mode_sign == '+':
                     sum_dice += dice_mode
@@ -45,15 +41,15 @@ def Dice(dice_code):
     else:
         return False
 
-print(Dice('19D100+1000'))
-print(Dice("2D10+10"))
-print(Dice("D6"))
-print(Dice("2D3"))
-print(Dice("D12-100"))
-print(Dice("019D100+1000"))
-print(Dice("15D200+1000"))
-print(Dice("15D100+0100"))
-print(Dice("lambada"))
-print(Dice(185))
 
-print(Dice("2D6-3"))
+print(dice('19D100+1000'))
+print(dice("2D10+10"))
+print(dice("D6"))
+print(dice("2D3"))
+print(dice("D12-100"))
+print(dice("019D100+1000"))
+print(dice("15D200+1000"))
+print(dice("15D100+0100"))
+print(dice("lambada"))
+print(dice(185))
+print(dice("2D6-3"))
